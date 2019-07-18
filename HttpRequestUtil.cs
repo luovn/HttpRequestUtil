94879c88a6d61481d2b11a5b1ce5c5352b4b20b7
yiyedi <1781131077@qq.com>
2019-07-18 17:13:12 +0800

    Create HttpRequestUtil.cs

---
+++ HttpRequestUtil.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Collections;
+using System.Web;
+using System.Text.RegularExpressions;
+namespace Utils
+{
+    /// <summary>
+    /// HTTP请求工具类
+    /// </summary>
+    public class HttpRequestUtil : System.Web.UI.Page
+    {
+        /// <summary>
+        /// 获取页面html
+        /// </summary>
+        public static string GetPageHtml(string url)
+        {
+            // 设置参数
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)";
+            //发送请求并获取相应回应数据
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            Stream responseStream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(responseStream, Encoding.GetEncoding("gb2312"));
+            //返回结果网页（html）代码
+            string content = sr.ReadToEnd();
+            return content;
+        }
+        /// <summary>
+        /// 获取页面html
+        /// </summary>
+        public static string GetPageHtml_UTF8(string url)
+        {
+            // 设置参数
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            request.UserAgent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)";
+            //发送请求并获取相应回应数据
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            //直到request.GetResponse()程序才开始向目标网页发送Post请求
+            Stream responseStream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(responseStream, Encoding.UTF8);
+            //返回结果网页（html）代码
+            string content = sr.ReadToEnd();
+            return content;
+        }
+
+
+
+        /// <summary>
+        /// 保存远程文件到本地
+        /// </summary>
+        /// <param name="fileUri">URI地址</param>
+        /// <returns>上传后的路径</returns>
+        public static string remoteSaveAs(string fileUri)
+        {
+            WebClient client = new WebClient();
+            string fileExt = string.Empty; //文件扩展名，不含“.”
+            if (fileUri.LastIndexOf("=") == -1)
+            {
+                fileExt = "gif";
+            }
+            else
+            {
+                fileExt = GetFileExt(fileUri, "=");
+            }
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssffff") + "." + fileExt; //随机生成新的文件名
+            string upLoadPath = GetUpLoadPath(); //上传目录相对路径
+            string fullUpLoadPath = GetMapPath(upLoadPath); //上传目录的物理路径
+            string newFilePath = upLoadPath + newFileName; //上传后的路径
+            //检查上传的物理路径是否存在，不存在则创建
+            if (!Directory.Exists(fullUpLoadPath))
+            {
+                Directory.CreateDirectory(fullUpLoadPath);
+            }
+
+            try
+            {
+                client.DownloadFile(fileUri, fullUpLoadPath + newFileName);
+               
+               
+            }
+            catch
+            {
+                return string.Empty;
+            }
+            client.Dispose();
+            return newFilePath;
+        }
+        #region 截取字符长度
+        /// <summary>
+        /// 截取字符长度
+        /// </summary>
+        /// <param name="inputString">字符</param>
+        /// <param name="len">长度</param>
+        /// <returns></returns>
+        public static string CutString(string inputString, int len)
+        {
+            if (string.IsNullOrEmpty(inputString))
+                return "";
+            inputString = DropHTML(inputString);
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            int tempLen = 0;
+            string tempString = "";
+            byte[] s = ascii.GetBytes(inputString);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((int)s[i] == 63)
+                {
+                    tempLen += 2;
+                }
+                else
+                {
+                    tempLen += 1;
+                }
+
+                try
+                {
+                    tempString += inputString.Substring(i, 1);
+                }
+                catch
+                {
+                    break;
+                }
+
+                if (tempLen > len)
+                    break;
+            }
+            //如果截过则加上半个省略号 
+            byte[] mybyte = System.Text.Encoding.Default.GetBytes(inputString);
+            if (mybyte.Length > len)
+                tempString += "…";
+            return tempString;
+        }
+        #endregion
+        #region 清除HTML标记且返回相应的长度
+        public static string DropHTML(string Htmlstring, int strLen)
+        {
+            return CutString(DropHTML(Htmlstring), strLen);
+        }
+        #endregion
+        #region 清除HTML标记
+        public static string DropHTML(string Htmlstring)
+        {
+            if (string.IsNullOrEmpty(Htmlstring)) return "";
+            //删除脚本  
+            Htmlstring = Regex.Replace(Htmlstring, @"<script[^>]*?>.*?</script>", "", RegexOptions.IgnoreCase);
+            //删除HTML  
+            Htmlstring = Regex.Replace(Htmlstring, @"<(.[^>]*)>", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"([\r\n])[\s]+", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"-->", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"<!--.*", "", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(quot|#34);", "\"", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(amp|#38);", "&", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(lt|#60);", "<", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(gt|#62);", ">", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(nbsp|#160);", " ", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(iexcl|#161);", "\xa1", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(cent|#162);", "\xa2", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(pound|#163);", "\xa3", RegexOptions.IgnoreCase);
+            Htmlstring = Regex.Replace(Htmlstring, @"&(copy|#169);", "\xa9", RegexOptions.IgnoreCase);
+
+            Htmlstring = Regex.Replace(Htmlstring, @"&#(\d+);", "", RegexOptions.IgnoreCase);
+            Htmlstring.Replace("<", "");
+            Htmlstring.Replace(">", "");
+            Htmlstring.Replace("\r\n", "");
+            Htmlstring.Replace("&emsp;", "");
+            Htmlstring = HttpContext.Current.Server.HtmlEncode(Htmlstring).Trim();
+            return Htmlstring;
+        }
+        #endregion
+
+
+        #region URL请求数据
+        /// <summary>
+        /// HTTP POST方式请求数据
+        /// </summary>
+        /// <param name="url">URL.</param>
+        /// <param name="param">POST的数据</param>
+        /// <returns></returns>
+        public static string HttpPost(string url, string param)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.Timeout = 15000;
+            request.AllowAutoRedirect = false;
+
+            StreamWriter requestStream = null;
+            WebResponse response = null;
+            string responseStr = null;
+
+            try
+            {
+                requestStream = new StreamWriter(request.GetRequestStream());
+                requestStream.Write(param);
+                requestStream.Close();
+
+                response = request.GetResponse();
+                if (response != null)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                    responseStr = reader.ReadToEnd();
+                    reader.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                request = null;
+                requestStream = null;
+                response = null;
+            }
+
+            return responseStr;
+        }
+
+        /// <summary>
+        /// HTTP GET方式请求数据.
+        /// </summary>
+        /// <param name="url">URL.</param>
+        /// <returns></returns>
+        public static string HttpGet(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.Method = "GET";
+            //request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.Timeout = 15000;
+            request.AllowAutoRedirect = false;
+
+            WebResponse response = null;
+            string responseStr = null;
+
+            try
+            {
+                response = request.GetResponse();
+
+                if (response != null)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                    responseStr = reader.ReadToEnd();
+                    reader.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                request = null;
+                response = null;
+            }
+
+            return responseStr;
+        }
+
+        /// <summary>
+        /// 执行URL获取页面内容
+        /// </summary>
+        public static string UrlExecute(string urlPath)
+        {
+            if (string.IsNullOrEmpty(urlPath))
+            {
+                return "error";
+            }
+            StringWriter sw = new StringWriter();
+            try
+            {
+                HttpContext.Current.Server.Execute(urlPath, sw);
+                return sw.ToString();
+            }
+            catch (Exception)
+            {
+                return "error";
+            }
+            finally
+            {
+                sw.Close();
+                sw.Dispose();
+            }
+        }
+        #endregion
+        #region 私有方法
+        /// <summary>
+        /// 返回上传目录相对路径
+        /// </summary>
+        /// <param name="fileName">上传文件名</param>
+        private static string GetUpLoadPath()
+        {
+            string path = "/wx_images/"; //站点目录+上传目录
+           
+                //按年月日每天一个文件夹
+                   // path += DateTime.Now.ToString("yyyyMMdd");
+                
+               //按年月/日存入不同的文件夹
+                    path += DateTime.Now.ToString("yyyyMM") + "/" + DateTime.Now.ToString("dd");
+                   
+            
+            return path + "/";
+        }
+
+       
+
+        /// <summary>
+        /// 是否为图片文件
+        /// </summary>
+        /// <param name="_fileExt">文件扩展名，不含“.”</param>
+        private static bool IsImage(string _fileExt)
+        {
+            ArrayList al = new ArrayList();
+            al.Add("bmp");
+            al.Add("jpeg");
+            al.Add("jpg");
+            al.Add("gif");
+            al.Add("png");
+            if (al.Contains(_fileExt.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 检查是否为合法的上传文件
+        /// </summary>
+        private static bool CheckFileExt(string _fileExt)
+        {
+            //检查危险文件
+            string[] excExt = { "asp", "aspx", "ashx", "asa", "asmx", "asax", "php", "jsp", "htm", "html" };
+            for (int i = 0; i < excExt.Length; i++)
+            {
+                if (excExt[i].ToLower() == _fileExt.ToLower())
+                {
+                    return false;
+                }
+            }
+            //检查合法文件
+            string[] allowExt = ("bmp,jpeg,jpg,gif,png").Split(',');
+            for (int i = 0; i < allowExt.Length; i++)
+            {
+                if (allowExt[i].ToLower() == _fileExt.ToLower())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// 返回文件扩展名，不含“.”
+        /// </summary>
+        /// <param name="_filepath">文件全名称</param>
+        /// <returns>string</returns>
+        public  static string GetFileExt(string _filepath, string _ext)
+        {
+            if (string.IsNullOrEmpty(_filepath))
+            {
+                return "";
+            }
+            if (_filepath.LastIndexOf(_ext) > 0)
+            {
+                return _filepath.Substring(_filepath.LastIndexOf(_ext) + 1); //文件扩展名，不含“.”
+            }
+            return "";
+        }
+        #region 获得当前绝对路径
+        /// <summary>
+        /// 获得当前绝对路径
+        /// </summary>
+        /// <param name="strPath">指定的路径</param>
+        /// <returns>绝对路径</returns>
+        public static string GetMapPath(string strPath)
+        {
+            if (strPath.ToLower().StartsWith("http://"))
+            {
+                return strPath;
+            }
+            if (HttpContext.Current != null)
+            {
+                return HttpContext.Current.Server.MapPath(strPath);
+            }
+            else //非web程序引用
+            {
+                strPath = strPath.Replace("/", "\\");
+                if (strPath.StartsWith("\\"))
+                {
+                    strPath = strPath.Substring(strPath.IndexOf('\\', 1)).TrimStart('\\');
+                }
+                return System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, strPath);
+            }
+        }
+        #endregion
+        #endregion
+    }
+}
